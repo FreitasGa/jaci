5cc47c726e2fe474238fc1d80261354763609409
Gabriel Freitas <freitasga.01@gmail.com>
2024-05-25 16:21:04 -0300

    feat: add head to preventions page

--- app/prevention/page.tsx
+++ app/prevention/page.tsx
@@ -1,7 +1,53 @@
 export default function Prevention() {
   return (
     <main className="container flex flex-col min-h-[100dvh] mx-auto px-4 py-8 md:px-6 md:py-12">
-      <h1>Prevention</h1>
+      <section className="w-full py-12 md:py-24 lg:py-32">
+        <div className="space-y-4">
+          <h1 className="text-4xl font-bold tracking-tighter md:text-5xl/tight">
+            Prevenção e Combate
+          </h1>
+          <div className="space-y-2 text-gray-600 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+            <p>
+              Aqui, você encontrará informações essenciais sobre como prevenir e
+              combater essas três doenças transmitidas pelo mosquito Aedes
+              aegypti. A prevenção é a chave para evitar surtos e proteger a
+              saúde de todos.
+            </p>
+            <p>
+              Dengue, zika e chikungunya podem ser evitadas com medidas simples
+              e eficazes. Nesta página, compartilharemos estratégias comprovadas
+              e orientações práticas para ajudar você a manter sua família e
+              comunidade seguras.
+            </p>
+            <p>Continue lendo para saber mais sobre:</p>
+            <ul className="list-disc list-inside">
+              <li>
+                <b>Medidas de Prevenção:</b> Aprenda a identificar e eliminar
+                criadouros do mosquito, além de outras ações diárias que você
+                pode adotar para reduzir o risco de transmissão.
+              </li>
+              <li>
+                <b>Métodos de Combate:</b> Descubra as melhores práticas para
+                combater o mosquito Aedes aegypti, incluindo o uso de
+                repelentes, inseticidas e a importância da mobilização
+                comunitária.
+              </li>
+              <li>
+                <b>Educação e Conscientização:</b> Entenda como a educação e a
+                conscientização são fundamentais para o controle dessas doenças
+                e como você pode contribuir para a disseminação de informações
+                corretas e úteis.
+              </li>
+            </ul>
+            <p>
+              Nosso objetivo é fornecer informações precisas e práticas para que
+              você possa estar bem informado e capacitado a tomar as medidas
+              necessárias para proteger sua saúde e a de sua comunidade. Juntos,
+              podemos vencer a batalha contra dengue, zika e chikungunya.
+            </p>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
--- app/symptoms/page.tsx
+++ app/symptoms/page.tsx
@@ -10,19 +10,41 @@ export default function Symptoms() {
           <h1 className="text-4xl font-bold tracking-tighter md:text-5xl/tight">
             Sintomas
           </h1>
-          <div className="space-y-2">
-            <p className="text-gray-500 md:text-2xl/relaxed lg:text-lg/relaxed xl:text-2xl/relaxed">
+          <div className="space-y-2 text-gray-600 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+            <p>
               Aqui, você encontrará informações detalhadas sobre os sinais e
               sintomas dessas três doenças transmitidas pelo mosquito Aedes
               aegypti. Conhecer os sintomas é essencial para o reconhecimento
               precoce e o tratamento adequado.
             </p>
-            <p className="text-gray-500 md:text-2xl/relaxed lg:text-lg/relaxed xl:text-2xl/relaxed">
+            <p>
               Dengue, zika e chikungunya compartilham algumas características
               comuns, mas também apresentam sintomas específicos que as
               diferenciam. Nesta página, exploraremos cada uma dessas condições
               de maneira clara e objetiva, ajudando você a identificar os sinais
               de cada doença.
+            </p>
+            <p>Continue lendo para saber mais sobre os sintomas de:</p>
+            <ul className="list-disc list-inside">
+              <li>
+                <b>Dengue:</b> Conheça os sintomas clássicos e os sinais de
+                alerta que podem indicar complicações.
+              </li>
+              <li>
+                <b>Zika:</b> Descubra os principais sintomas, incluindo aqueles
+                que são mais leves e frequentemente confundidos com outras
+                condições.
+              </li>
+              <li>
+                <b>Chikungunya:</b> Entenda os sinais típicos e como eles se
+                manifestam de maneira diferente em relação às outras duas
+                doenças.
+              </li>
+            </ul>
+            <p>
+              Nosso objetivo é fornecer informações precisas e úteis para que
+              você possa estar bem informado e preparado para tomar as medidas
+              necessárias caso surjam sintomas.
             </p>
           </div>
         </div>
