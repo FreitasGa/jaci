5d3343095a275ef9752a82c15a7d470ab8eb7d1e
LucasXXI <lucasleal2001@gmail.com>
2024-05-25 15:30:48 -0300

    feat: adding new symptoms to the data file.

--- app/symptoms/data.ts
+++ app/symptoms/data.ts
@@ -12,6 +12,47 @@ export const diseases: Disease[] = [
         description:
           "Geralmente acima de 38,5°C, de início repentino e dura de 2 a 7 dias.",
       },
+      {
+        name: "Artralgia (Dor nas Articulações)",
+        description: "Dor nas articulações que pode durar semanas. Quase sempre presente e com dores moderadas."
+      },
+      {
+        name: "Rash Cutâneo (Manchas Vermelhas na Pele)",
+        description: "Erupção cutânea com manchas vermelhas na pele de início súbito e que dura de 2 a 5 dias. Pode estar presente dentre os sintomas mas nem sempre ocorre."
+      },
+      {
+        name: "Coceira (Prurido Cutâneo)",
+        description: "Coceira na pele que pode durar dias. Pode estar presente dentre os sintomas de maneira leve."
+      },
+      {
+        name: "Mialgia (Dor Muscular)",
+        description: "Dor muscular que pode durar semanas. Quase sempre presente e com dores moderadas."
+      },
+      {
+        name: "Cefaleia (Dor de Cabeça)",
+        description: "Dor de cabeça que pode durar dias. Quase sempre presente e com dores moderadas."
+      },
+      //Gerado automaticamente (se quiser tirar so apagar)
+      {
+        name: "Dor Retroorbital (Dor Atrás dos Olhos)",
+        description: "Dor atrás dos olhos que pode durar dias. Quase sempre presente e com dores moderadas."
+      },
+      {
+        name: "Anorexia (Perda de Apetite)",
+        description: "Perda de apetite que pode durar dias. Quase sempre presente e com dores moderadas."
+      },
+      {
+        name: "Náusea (Enjoo)",
+        description: "Enjoo que pode durar dias. Quase sempre presente e com dores moderadas."
+      },
+      {
+        name: "Vômito",
+        description: "Vômito que pode durar dias. Quase sempre presente e com dores moderadas."
+      },
+      {
+        name: "Dor Abdominal",
+        description: "Dor abdominal que pode durar dias. Quase sempre presente e com dores moderadas."
+      },
     ],
   },
   {
@@ -23,6 +64,52 @@ export const diseases: Disease[] = [
       {
         name: "Febre Baixa",
         description: "Geralmente abaixo de 38°C, que dura de 2 a 7 dias.",
+      },
+      {
+        name: "Artralgia (Dor nas Articulações)",
+        description: "Dor nas articulações, que pode estar presente e apresentar dores leves",
+      },
+      {
+        name: "Rash Cutâneo (Manchas Vermelhas na Pele)",
+        description:
+          "Erupção cutânea com manchas vermelhas na pele de início súbito e que dura de 2 a 5 dias.",
+      },
+      {
+        name: "Prurido (Coceira)",
+        description: "Coceira na pele que pode durar dias. Pode estar presente e em grau leve ou intenso",
+      },
+      {
+        name: "Vermelhidão nos olhos (Hiperemia Conjuntival)",
+        description: "Vermelhidão nos olhos que pode durar dias. Nem sempre presente e com dores leves.",
+      },
+      //gerado automaticamente (se quiser tirar so apagar)
+      {
+        name: "Mialgia (Dor Muscular)",
+        description: "Dor muscular que pode durar semanas.",
+      },
+      {
+        name: "Cefaleia (Dor de Cabeça)",
+        description: "Dor de cabeça que pode durar dias.",
+      },
+      {
+        name: "Dor Retroorbital (Dor Atrás dos Olhos)",
+        description: "Dor atrás dos olhos que pode durar dias.",
+      },
+      {
+        name: "Anorexia (Perda de Apetite)",
+        description: "Perda de apetite que pode durar dias.",
+      },
+      {
+        name: "Náusea (Enjoo)",
+        description: "Enjoo que pode durar dias.",
+      },
+      {
+        name: "Vômito",
+        description: "Vômito que pode durar dias.",
+      },
+      {
+        name: "Dor Abdominal",
+        description: "Dor abdominal que pode durar dias.",
       },
     ],
   },
@@ -36,6 +123,52 @@ export const diseases: Disease[] = [
         name: "Febre Alta",
         description:
           "Geralmente acima de 38,5°C, de início repentino e dura de 2 a 7 dias.",
+      },
+      {
+        name: "Artralgia (Dor nas Articulações)",
+        description: "Dor intensa nas articulações, que é presente em 90% dos casos.",
+      },
+      {
+        name: "Rash Cutâneo (Manchas Vermelhas na Pele)",
+        description:
+          "Erupção cutânea com manchas vermelhas na pele de início súbito e que dura de 2 a 5 dias. Se manifesta nas primeiras 48hr (a partir do secundo dia).",
+      },
+      {
+        name: "Prurido (Coceira)",
+        description: "Coceira na pele que pode durar dias. Pode estar presente e em grau leve e está presente em 50 a 80% dos casos.",
+      },
+      {
+        name: "Vermelhidão nos olhos (Hiperemia Conjuntival)",
+        description: "Vermelhidão nos olhos que pode durar dias. Nem sempre presente e com dores leves.",
+      },
+      //gerado automaticamente (se quiser tirar so apagar)
+      {
+        name: "Mialgia (Dor Muscular)",
+        description: "Dor muscular que pode durar semanas.",
+      },
+      {
+        name: "Cefaleia (Dor de Cabeça)",
+        description: "Dor de cabeça que pode durar dias.",
+      },
+      {
+        name: "Dor Retroorbital (Dor Atrás dos Olhos)",
+        description: "Dor atrás dos olhos que pode durar dias.",
+      },
+      {
+        name: "Anorexia (Perda de Apetite)",
+        description: "Perda de apetite que pode durar dias.",
+      },
+      {
+        name: "Náusea (Enjoo)",
+        description: "Enjoo que pode durar dias.",
+      },
+      {
+        name: "Vômito",
+        description: "Vômito que pode durar dias.",
+      },
+      {
+        name: "Dor Abdominal",
+        description: "Dor abdominal que pode durar dias.",
       },
     ],
   },
