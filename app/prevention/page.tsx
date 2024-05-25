1a4dde0893acae3f9eb1381fb03ed3379ecec924
Gabriel Freitas <freitasga.01@gmail.com>
2024-05-24 21:46:54 -0300

    feat: add prevention and symptoms pages

---
+++ app/prevention/page.tsx
@@ -0,0 +1,7 @@
+export default function Prevention() {
+  return (
+    <main className="container flex flex-col min-h-[100dvh] mx-auto px-4 py-8 md:px-6 md:py-12">
+      <h1>Prevention</h1>
+    </main>
+  );
+}
