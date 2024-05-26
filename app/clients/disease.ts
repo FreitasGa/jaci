aedeeb090fb1f0ed67f4244b5111a0bfcce91039
Gabriel Freitas <freitasga.01@gmail.com>
2024-05-26 18:53:53 -0300

    refactor: remove app url env

--- app/clients/disease.ts
+++ app/clients/disease.ts
@@ -1,10 +1,7 @@
 import { DiseaseData } from "../types";
 
 export default async function fetchDiseaseData(): Promise<DiseaseData | null> {
-  console.log(process.env.NEXT_PUBLIC_API_URL);
-  const url = process.env.NEXT_PUBLIC_API_URL + "/api/disease";
-
-  const response = await fetch(url, {
+  const response = await fetch("/api/disease", {
     headers: {
       "Content-Type": "application/json",
     },
